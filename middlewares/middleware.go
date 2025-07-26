43b13b5303aac5ccac46b43bb424507f1f6b2247
rzqiiqmal <rizqiiqmal805@gmail.com>
2025-07-26 15:35:57 +0700

    balikin ke sebelum diubah

--- middlewares/middleware.go
+++ middlewares/middleware.go
@@ -1,16 +1,3 @@
-// package middlewares
-
-// import (
-// 	"github.com/gofiber/fiber/v2"
-// 	"github.com/gofiber/fiber/v2/middleware/cors"
-// 	"github.com/gofiber/fiber/v2/middleware/logger"
-// )
-
-// func SetupMiddleware(app *fiber.App) {
-// 	app.Use(cors.New())
-// 	app.Use(logger.New())
-// }
-
 package middlewares
 
 import (
@@ -20,10 +7,6 @@ import (
 )
 
 func SetupMiddleware(app *fiber.App) {
-	app.Use(cors.New(cors.Config{
-		AllowOrigins: "https://feinventory-production.up.railway.app/",
-		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
-	}))
-
+	app.Use(cors.New())
 	app.Use(logger.New())
 }
