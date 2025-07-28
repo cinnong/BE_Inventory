758348bc4af28cad58f066fad59c145b3be7f163
Dina Oktafiani <dinaoktafiani04@gmail.com>
2025-07-28 14:19:37 +0700

    update

--- middlewares/middleware.go
+++ middlewares/middleware.go
@@ -8,7 +8,7 @@ import (
 
 func SetupMiddleware(app *fiber.App) {
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "https://feinventory-production.up.railway.app, http://localhost:5173, http://localhost:5174", // contoh domain react kamu di railway
+		AllowOrigins:     "https://feinventory-production.up.railway.app, http://localhost:5173, http://localhost:5174, http://localhost:3000, http://localhost", // tambahkan origin swagger dan localhost
 		AllowCredentials: true,
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 	}))
